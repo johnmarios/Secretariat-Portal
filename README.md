3123a7225a46aab358a9a77575c2f4a9e59b26b9
Alexi <alexisloridas@gmail.com>
2026-05-28 21:13:13 +0300

    Διαγραφή πολλών διπλών files, εξυπνο pool στο db.js για αυκολη εναλλαγη απο local βαση σε cloud

---
+++ README.md
@@ -0,0 +1,13 @@
+Clone:
+git clone https://github.com/johnmarios/Secretariat-Portal.git
+    
+Εγκατάσταση εξαρτήσεων στο Terminal:
+cmd
+npm install
+npm start
+
+Email-Passwords για Λογαριασμούς βρίσκονται στο seed-credentials.txt
+
+Live link:
+https://secretariat-portal.onrender.com/
+
--- model/db.js
+++ model/db.js
@@ -5,24 +5,13 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-// const pool = mysql.createPool({
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASS,
-//     database: process.env.DB_NAME,
-// });
-
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD, 
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT,       
-    
-    ssl: {
-        rejectUnauthorized: false
-    },
-    
+    port: process.env.DB_PORT,           
+    ssl: process.env.DB_HOST === 'localhost' ? undefined : { rejectUnauthorized: false },    
     waitForConnections: true,
     connectionLimit: 10
 });
